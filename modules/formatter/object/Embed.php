092abd2cb596dcf940d4bdd2258c83c71d1df9d6
Iqbal Fauzi <iqbalfawz@gmail.com>
2017-09-29 12:39:35 +0700

    Rewrite embed script

--- modules/formatter/object/Embed.php
+++ modules/formatter/object/Embed.php
@@ -13,129 +13,237 @@ class Embed implements \JsonSerializable
     protected $_value;
     
     protected $height;
-    protected $vendor;
     protected $html;
-    protected $width;
-    protected $url;
     protected $size;
-        
+    protected $url;
+    protected $vendor;
+    protected $width;
+    
+    protected $mime;
+    protected $user;
+    
     private function _parseEmbed(){
+        $tmpls = [
+            'dailymail' => 
+                  '<iframe '
+                .   'allowFullscreen="1" '
+                .   'frameborder="0" '
+                .   'height="${height}" '
+                .   'scrolling="auto" '
+                .   'src="${url}" '
+                .   'width="${width}">'
+                . '</iframe>',
+            
+            'dailymotion' => 
+                  '<iframe '
+                .   'allowFullscreen="1" '
+                .   'frameborder="0" '
+                .   'height="${height}" '
+                .   'scrolling="no" '
+                .   'src="${url}" '
+                .   'width="${width}">'
+                . '</iframe>',
+            
+            'facebook' =>
+                  '<div '
+                .   'class="fb-video" '
+                .   'data-allowfullscreen="true" '
+                .   'data-href="${url}" '
+                .   'data-show-text="false" '
+                .   'data-size="${size}" '
+                .   'data-width="auto">'
+                . '</div>',
+            
+            'imdb' => 
+                  '<iframe '
+                .   'allowFullscreen="1" '
+                .   'frameborder="0" '
+                .   'height="${height}" '
+                .   'scrolling="no" '
+                .   'src="${url}" '
+                .   'width="${width}">'
+                . '</iframe>',
+            
+            'instagram' => 
+                  '<blockquote '
+                .   'class="instagram-media" '
+                .   'data-instgrm-captioned '
+                .   'data-instgrm-version="7">'
+                .     '<a '
+                .       'href="${url}" '
+                .       'target="_blank">'
+                .     '</a>'
+                . '</blockquote>',
+            
+            'liveleak' => 
+                  '<iframe '
+                .   'allowFullscreen="1" '
+                .   'frameborder="0" '
+                .   'height="${height}" '
+                .   'scrolling="no" '
+                .   'src="${url}" '
+                .   'width="${width}">'
+                . '</iframe>',
+            
+            'vidio' => 
+                  '<iframe '
+                .   'allowFullscreen="1" '
+                .   'frameborder="0" '
+                .   'height="${height}" '
+                .   'scrolling="no" '
+                .   'src="${url}" '
+                .   'width="${width}">'
+                . '</iframe>',
+            
+            'vidme' => 
+                  '<iframe '
+                .   'allowFullscreen="1" '
+                .   'frameborder="0" '
+                .   'height="${height}" '
+                .   'scrolling="no" '
+                .   'src="${url}" '
+                .   'width="${width}">'
+                . '</iframe>',
+            
+            'vimeo' => 
+                  '<iframe '
+                .   'allowFullscreen="1" '
+                .   'frameborder="0" '
+                .   'height="${height}" '
+                .   'scrolling="no" '
+                .   'src="${url}" '
+                .   'width="${width}">'
+                . '</iframe>',
+            
+            'youtube' => 
+                  '<iframe '
+                .   'allowFullscreen="1" '
+                .   'frameborder="0" '
+                .   'height="${height}" '
+                .   'scrolling="no" '
+                .   'src="${url}" '
+                .   'width="${width}">'
+                . '</iframe>',
+            
+            'videoplayer' => 
+                  '<video '
+                .   'width="${width}" '
+                .   'height="${height}" '
+                .   'controls>'
+                .     '<source src="${url}" type="${mime}">'
+                . '</video>'
+        ];
+        
+        $urls = [
+            'dailymail'     => 'http://www.dailymail.co.uk/embed/video/${id}.html',
+            'dailymotion'   => 'https://www.dailymotion.com/embed/video/${id}',
+            'facebook'      => 'https://www.facebook.com/${user}/videos/${id}',
+            'imdb'          => 'http://www.imdb.com/videoembed/${id}',
+            'instagram'     => 'https://www.instagram.com/p/${id}',
+            'liveleak'      => 'http://www.liveleak.com/ll_embed?f=${id}',
+            'videoplayer'   => '${url}',
+            'vidio'         => 'https://www.vidio.com/embed/${id}?player_only=true&autoplay=false',
+            'vidme'         => 'https://vid.me/e/${id}?tools=1',
+            'vimeo'         => 'https://player.vimeo.com/video/${id}?title=0&amp;byline=0&amp;portrait=0&color=e3a01b',
+            'youtube'       => 'https://www.youtube.com/embed/${id}?rel=0'
+        ];
+        
+        $regexs = [
+            '/youtube.com\/embed\/([\w_\-]+)/i'                 => [ 'youtube',     ['id'=>1]               ],
+            '/youtube\.com(.+)v=([\w_\-]+)/'                    => [ 'youtube',     ['id'=>2]               ],
+            '/facebook\.com\/([^\/]+)\/videos\/([^\/]+)/'       => [ 'facebook',    ['user'=>1,'id'=>2]     ],
+            '/facebook\.com\/.+facebook\.com%2F([^%]+)%2Fvideos%2F([0-9]+)/'
+                                                                => [ 'facebook',    ['user'=>1,'id'=>2]     ],
+            '/youtu\.be\/([\w_\-]+)/'                           => [ 'youtube',     ['id'=>1]               ],
+            '/^.+\.(mp4|mpeg|ogg|webm)$/i'                      => [ 'videoplayer', ['url'=>0, 'mime'=>1]   ],
+        
+            '/vidio.com\/embed\/([\w\-]+)/'                     => [ 'vidio',       ['id'=>1]               ],
+            '/vidio.com\/watch\/([\w\-]+)/'                     => [ 'vidio',       ['id'=>1]               ],
+            '/instagram\.com\/p\/(\w+)/'                        => [ 'instagram',   ['id'=>1]               ],
+            '/dailymail.co.uk\/video\/([\w]+)\/video-([0-9]+)/' => [ 'dailymail',   ['id'=>2]               ],
+            '/dailymail.co.uk\/([\w]+)\/video\/([0-9]+)/'       => [ 'dailymail',   ['id'=>2]               ],
+            '/dailymotion.com\/embed\/video\/([a-z0-9]+)/'      => [ 'dailymotion', ['id'=>1]               ],
+            '/dailymotion.com\/video\/([a-z0-9]+)/'             => [ 'dailymotion', ['id'=>1]               ],
+            '/dailymotion.com\/.+#video=([a-z0-9]+)/'           => [ 'dailymotion', ['id'=>1]               ],
+            '/dai\.ly\/([a-z0-9]+)/'                            => [ 'dailymotion', ['id'=>1]               ],
+            '/imdb\.com\/videoembed\/([\w]+)/'                  => [ 'imdb',        ['id'=>1]               ],
+            '/imdb\.com\/videoplayer\/([\w]+)/'                 => [ 'imdb',        ['id'=>1]               ],
+            '/imdb\.com\/.*\/videoplayer\/([\w]+)/'             => [ 'imdb',        ['id'=>1]               ],
+            '/imdb\.com\/video\/imdb\/([\w]+)/'                 => [ 'imdb',        ['id'=>1]               ],
+            '/liveleak.com\/ll_embed\?f=([\w\-]+)/'             => [ 'liveleak',    ['id'=>1]               ],
+            '/vid.me\/e\/([\w\-]+)/'                            => [ 'vidme',       ['id'=>1]               ],
+            '/vid.me\/([\w\-]+)/'                               => [ 'vidme',       ['id'=>1]               ],
+            '/vimeo\.com\/([0-9]+)/'                            => [ 'vimeo',       ['id'=>1]               ],
+            '/vimeo\.com\/(.*)\/([0-9]+)/'                      => [ 'vimeo',       ['id'=>2]               ],
+            '/youtube-nocookie.com\/embed\/([\w_\-]+)/i'        => [ 'youtube',     ['id'=>1]               ],
+            
+        ];
+        
         $sizes = [
             'dailymail'     => [ 'width' => 698, 'height' => 573 ],
             'dailymotion'   => [ 'width' => 480, 'height' => 270 ],
-            'imdb'          => [ 'width' => 854, 'height' => 400 ],
+            'imdb'          => [ 'width' => 854, 'height' => 650 ],
+            'instagram'     => [ 'width' => 320, 'height' => 320 ],
             'facebook'      => [ 'width' => 854, 'height' => 400 ],
             'liveleak'      => [ 'width' => 640, 'height' => 360 ],
+            'videoplayer'   => [ 'width' => 560, 'height' => 314 ],
             'vidio'         => [ 'width' => 480, 'height' => 270 ],
             'vidme'         => [ 'width' => 854, 'height' => 480 ],
-            'vimeo'         => [ 'width' => 425, 'height' => 350 ],
+            'vimeo'         => [ 'width' => 560, 'height' => 314 ],
             'youtube'       => [ 'width' => 560, 'height' => 314 ],
         ];
         
-        $vendors = [
-            'dailymail.co.uk'       => 'dailymail',
-            'dailymotion.com'       => 'dailymotion',
-            'facebook.com'          => 'facebook',
-            'imdb.com'              => 'imdb',
-            'liveleak.com'          => 'liveleak',
-            'vidio.com'             => 'vidio',
-            'vimeo.com'             => 'vimeo',
-            'player.vimeo.com'      => 'vimeo',
-            'youtu.be'              => 'youtube',
-            'youtube.com'           => 'youtube',
-            'youtube-nocookie.com'  => 'youtube',
-            'vid.me'                => 'vidme'
-        ];
+        $props = ['width', 'height', 'user', 'id', 'url', 'vendor', 'size', 'mime'];
         
-        $re_ids = [
-            '/youtube.com\/embed\/([\w_\-]+)/i'                 => 1,
-            '/youtube\.com(.+)v=([\w_\-]+)/'                    => 2,
-            '/facebook\.com\/([^\/]+)\/videos\/([^\/]+)/'       => 2,
-            '/vidio.com\/embed\/([\w\-]+)/'                     => 1,
-            
-            '/dailymail.co.uk\/video\/([\w]+)\/video-([0-9]+)/' => 2,
-            '/dailymail.co.uk\/([\w]+)\/video\/([0-9]+)/'       => 2,
-            '/dailymotion.com\/video\/([^_]+)/'                 => 1,
-            '/dailymotion.com\/embed\/video\/([^_]+)/'          => 1,
-            '/imdb\.com\/videoembed\/([\w]+)/'                  => 1,
-            '/imdb\.com\/video\/imdb\/([\w]+)/'                 => 1,
-            '/imdb\.com\/list\/[^\/]+\/videoplayer\/([\w]+)/'   => 1,
-            '/liveleak.com\/ll_embed\?f=([\w\-]+)/'             => 1,
-            '/vid.me\/([\w\-]+)/'                               => 1,
-            '/vid.me\/e\/([\w\-]+)/'                            => 1,
-            '/vidio.com\/watch\/([\w\-]+)/'                     => 1,
-            '/vimeo\.com\/([0-9]+)/'                            => 1,
-            '/vimeo\.com\/(.*)\/([0-9]+)/'                      => 2,
-            '/youtu\.be\/([\w_\-]+)/'                           => 1,
-            '/youtube-nocookie.com\/embed\/([\w_\-]+)/i'        => 1
-        ];
+        $this->url = trim($this->_value);
+        $this->html= $this->url;
         
-        $templates = [
-            'dailymail'     => '<iframe width="${width}" height="${height}" allowFullscreen="1" frameborder="0" scrolling="no" src="http://www.dailymail.co.uk/embed/video/${id}.html"></iframe>',
-            'dailymotion'   => '<iframe width="${width}" height="${height}" allowFullscreen="1" frameborder="0" src="https://www.dailymotion.com/embed/video/${id}"></iframe>',
-            'facebook'      => '<div class="fb-video" data-href="${url}" data-width="auto" data-size="${size}" data-show-text="false" data-allowfullscreen="true"></div>',
-            'imdb'          => '<iframe width="${width}" height="${height}" allowFullscreen="1" frameborder="0" src="http://www.imdb.com/videoembed/${id}"></iframe>',
-            'liveleak'      => '<iframe width="${width}" height="${height}" allowFullscreen="1" frameborder="0" src="http://www.liveleak.com/ll_embed?f=${id}"></iframe>',
-            'vidio'         => '<iframe width="${width}" height="${height}" allowFullscreen="1" frameborder="0" src="https://www.vidio.com/embed/${id}?player_only=true&autoplay=false"></iframe>',
-            'vidme'         => '<iframe width="${width}" height="${height}" allowFullscreen="1" frameborder="0" src="https://vid.me/e/${id}?tools=1"></iframe>',
-            'vimeo'         => '<iframe width="${width}" height="${height}" allowFullscreen="1" frameborder="0" src="https://player.vimeo.com/video/${id}?title=0&amp;byline=0&amp;portrait=0&color=e3a01b"></iframe>',
-            'youtube'       => '<iframe width="${width}" height="${height}" allowFullscreen="1" frameborder="0" src="https://www.youtube.com/embed/${id}?rel=0"></iframe>'
-        ];
-        
-        $this->url  = trim($this->_value);
-        $this->html = $this->url;
         if(!$this->url)
             return;
         
-        $val = $this->url;
-        if(preg_match('!<(iframe|video)[^>]+src="([^"]+)"!', $val, $match)){
-            $this->url = $match[2];
-            
-            if(preg_match('!width="([0-9]+)"!', $val, $match))
-                $this->width = $match[1];
-            if(preg_match('!height="([0-9]+)"!', $val, $match))
-                $this->height = $match[1];
-        }
-        
-        $url = parse_url($this->url);
-        
-        // let find the vendor
-        $url['host'] = preg_replace('!^www\.!', '', $url['host']);
-        $this->vendor = $vendors[$url['host']] ?? null;
-        
-        // facebook.com/plugins/video.php?href=.
-        if($this->vendor == 'facebook' && $url['path'] == '/plugins/video.php' && $url['query']){
-            parse_str($url['query'], $url_query);
-            if(isset($url_query['href']))
-                $this->url = $url_query['href'];
-            if(isset($url_query['width']))
-                $this->width = $url_query['width'];
-            if(isset($url_query['height']))
-                $this->height = $url_query['height'];
-        }
-        
-        if($this->vendor){
-            if(!$this->width)
-                $this->width = $sizes[$this->vendor]['width'];
-            if(!$this->height)
-                $this->height = $sizes[$this->vendor]['height'];
-        }
-        
-        foreach($re_ids as $re => $index){
-            if(preg_match($re, $this->url, $match) && isset($match[$index])){
-                $this->id = $match[$index];
+        $match = null;
+        foreach($regexs as $re => $rule){
+            if(preg_match($re, $this->url, $match))
                 break;
-            }
+        }
+        
+        if(!$match)
+            return;
+        
+        $this->vendor = $rule[0];
+        foreach($rule[1] as $prop => $index)
+            $this->$prop = $match[$index];
+        
+        $url = $urls[$this->vendor];
+        foreach($props as $attr)
+            $url = str_replace('${'.$attr.'}', $this->$attr, $url);
+        $this->url = $url;
+        
+        if(preg_match('!width ?[:|=][ "]?([0-9]+)!', $this->html, $match))
+            $this->width = $match[1] ? $match[1] : null;
+        if(preg_match('!height ?[:|=][ "]?([0-9]+)!', $this->html, $match))
+            $this->height = $match[1] ? $match[1] : null;
+        
+        if(!$this->width && !$this->height){
+            $this->width  = $sizes[$this->vendor]['width'];
+            $this->height = $sizes[$this->vendor]['height'];
+        
+        }elseif(!$this->width && $this->height){
+            $this->width  = floor($this->height * $sizes[$this->vendor]['width'] / $sizes[$this->vendor]['height'] );
+            
+        }elseif($this->width && !$this->height){
+            $this->height = floor($this->width * $sizes[$this->vendor]['height'] / $sizes[$this->vendor]['width'] );
+            
         }
         
         $this->size = $this->width . 'x' . $this->height;
-        if(isset($templates[$this->vendor])){
-            $template = $templates[$this->vendor];
-            $looper   = ['width', 'height', 'id', 'url', 'vendor', 'size'];
-            foreach($looper as $loop)
-                $template = str_replace('${'.$loop.'}', $this->$loop, $template);
-            $this->html = $template;
-        }
+        if($this->mime)
+            $this->mime = 'video/'.$this->mime;
+        
+        $html = $tmpls[$this->vendor];
+        foreach($props as $attr)
+            $html = str_replace('${'.$attr.'}', $this->$attr, $html);
+        $this->html = $html;
     }
     
     public function __construct($value){
